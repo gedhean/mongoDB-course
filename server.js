11ef84628f9ab7a1e8b65325f37167f4baf1b365
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-03-31 14:04:28 -0300

    test templates

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+const engines = require('consolidate');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-    res.end('Hello World!');
+    res.render('index', {'name': 'jedi'});
 });
 
 app.use((req, res) => {
     res.sendStatus(404);
 });
 
-app.listen(3000, 'localhost', () => {
+const server = app.listen(3000, 'localhost', () => {
     console.log('Listening on port 3000');
 });
