1bf1a50bcf7feb5e3c2686a0efc96c91b009499f
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-03-31 15:14:20 -0300

    more tests

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const express = require('express');
+const app = express();
+const engines = require('consolidate');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     assert.equal(null, err);
@@ -7,13 +14,20 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
 
     const db = client.db('local');
 
-    db.collection('users').find().toArray(function (err, result) {
-        assert.equal(null, err);
-        console.log('Results founded:');
-        result.forEach(element => {
-            console.log(element);
+    app.get('/', (req, res) => {
+        db.collection('users').find().toArray(function (err, result) {
+            assert.equal(null, err);
+            res.render('users', {'users': result});
         });
     });
+    
+    app.use((req, res) => {
+        res.sendStatus(404);
+    });
+    
+    const server = app.listen(3000, 'localhost', () => {
+        console.log('Listening on port 3000');
+    });
 
-    client.close();
+    //client.close();
 })
